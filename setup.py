f8b418fdfe2feb63d307decd383ba7389c8b6946
Jonathannavaxd <62471540+Jonathannavaxd@users.noreply.github.com>
2024-11-19 15:17:57 -0400

    add

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyimagelocator',
     version='0.1.0',
     author='Jhonatan Navarro',
     author_email='Jonathannavaxd@gmail.com',
     description='It was created out of the need to locate RGBA images with alpha background in given images.',
-    packages=find_packages(),
-    install_requires=[],  # Aquí puedes agregar dependencias si las tienes
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Jonathannavaxd/ImageLocator',  # Cambia esto a la URL de tu proyecto
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
