43bdce48c577bac2144195a53ddc642a18c4d4b4
Jonathannavaxd <62471540+Jonathannavaxd@users.noreply.github.com>
2024-11-19 15:52:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Windows',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.12.4',
 )
